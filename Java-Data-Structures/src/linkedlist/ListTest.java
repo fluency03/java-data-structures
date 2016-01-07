cad3110309dce8397dce639855a859ff5cad1cab
cliu <fluency.03@gmail.com>
2016-01-07 22:53:32 +0100

    Linked List

--- Java-Data-Structures/src/linkedlist/ListTest.java
+++ Java-Data-Structures/src/linkedlist/ListTest.java
@@ -8,19 +8,40 @@ package linkedlist;
 public class ListTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+	  // ListNode test
 		ListNode<Integer> ln1 = new ListNode<>(5);
 		ListNode<Integer> ln2 = new ListNode<>();
 		ln2.setData(15);
 		System.out.println("The data of the node: " + ln1.getData() + " " + ln2.getData());
 		
 		
+		// SingleLinkedList test
+		SingleLinkedList<Integer> sll = new SingleLinkedList<>(); 
+		for (int i = 0; i < 10; i++) {
+		  ListNode<Integer> ln = new ListNode<>(i);
+		  sll.insertAtBegin(ln);
+    }
 		
+    for (int i = 10; i < 20; i++) {
+      ListNode<Integer> ln = new ListNode<>(i);
+      sll.insertAtEnd(ln);
+    }
 		
-		
-		
-		
-		
+    sll.insert(100, 5);
+    
+    ListNode<Integer> p, q;
+    for (p=sll.getHead(); (q = p.getNext()) != null; p=q) {
+      // this condition of for-statement is not correct
+      System.out.println( p.getData() + " " + (p.getNext()==null) );
+    }
+    for (p=sll.getHead(); p != null; p=p.getNext()) {
+      System.out.println( p.getData() + " " + (p.getNext()==null) );
+    }
+    
+    System.out.println( sll.getLength() );
+
+
 		
 		
 		
--- Java-Data-Structures/src/linkedlist/SingleLinkedList.java
+++ Java-Data-Structures/src/linkedlist/SingleLinkedList.java
@@ -13,17 +13,91 @@ public class SingleLinkedList<T> {
   private ListNode<T> tail = null;
   private int length = 0;
   
-  public SingleLinkedList() {
-    length = 0;
+  public SingleLinkedList() { }
+  
+  public SingleLinkedList(ListNode<T> node) {
+    head = node;
+    tail = node;
   }
   
-  
+  // Return the head of list
   public ListNode<T> getHead() {
     return head;
   }
   
+  // Return the head of list
+  public ListNode<T> getTail() {
+    return tail;
+  }
   
+  // Return the length of list
+  public int getLength() {
+    return length;
+  }
+  
+  // Insert a node at beginning of the list
+  public void insertAtBegin(ListNode<T> node) {
+    if (head == null || tail == null) {
+      head = node;
+      tail = node;
+    } else {
+      node.setNext(head);
+      head = node;
+    } 
+    
+    length ++;
+  }
 
-
+  // Insert a node at the end of the list
+  public void insertAtEnd(ListNode<T> node) {
+    if (head == null || tail == null) {
+      head = node;
+      tail = node;
+    } else {
+      ListNode<T> last = tail;
+      last.setNext(node);
+      tail = node;
+    }
+    
+    length ++;
+  }
+  
+  public boolean insert(T data, int position ) {
+    // Check the position
+    if (position < 0 || position > length) {
+      System.out.println("The position is out of range! ");
+      return false;
+    }
+    
+    if (head == null || tail == null) {
+      head = new ListNode<T>(data);
+      tail = head;
+    } else if (position == 0) {
+      ListNode<T> node = new ListNode<>(data);
+      node.setNext(head);
+      head = node;
+    } else {
+      ListNode<T> node = head;
+      ListNode<T> newNode = new ListNode<>(data);
+      
+      for (int i=1; i<position; i++) {
+        node = node.getNext();
+      }
+      
+      newNode.setNext(node.getNext());
+      node.setNext(newNode);
+    }
+    
+    length ++;
+    return true;
+  }
+  
+  
+  
+  
+  
+  
+  
+  
 
 }
