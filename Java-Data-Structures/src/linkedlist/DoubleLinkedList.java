f0f4e1759b474514623a78ccfccac9de71abd58c
cliu <fluency.03@gmail.com>
2016-01-08 13:45:52 +0100

    LinkedList

--- Java-Data-Structures/src/linkedlist/DoubleLinkedList.java
+++ Java-Data-Structures/src/linkedlist/DoubleLinkedList.java
@@ -7,6 +7,11 @@ package linkedlist;
 
 public class DoubleLinkedList<T> {
 
+  private DoubleListNode<T> head = null;
+  private DoubleListNode<T> tail = null;
+  private int length = 0;
+  
+  
   
   
   
--- Java-Data-Structures/src/linkedlist/DoubleListNode.java
+++ Java-Data-Structures/src/linkedlist/DoubleListNode.java
@@ -23,30 +23,37 @@ public class DoubleListNode<T> {
     this.next = next;
   }
   
+  // Obtain the node's data
   public T getData(){
     return data;
   }
   
+  // Set the node's data
   public void setData(T data) {
     this.data = data;
   }
   
+  // Return the previous node
   public DoubleListNode<T> getPrev(){
     return prev;
   }
   
+  // Set previous node
   public void setPrev(DoubleListNode<T> prev) {
     this.prev = prev;
   }
   
+  // Return the next node
   public DoubleListNode<T> getNext(){
     return next;
   }
   
+  // Set the next node
   public void setNext(DoubleListNode<T> next) {
     this.next = next;
   }
   
+  // Convert the data into String
   public String toString() {
     String str = "";
     return str + data;
