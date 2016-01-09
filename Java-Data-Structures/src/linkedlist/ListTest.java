7ae6645a358550e9d0a99d7f79307141d7509db5
cliu <fluency.03@gmail.com>
2016-01-09 14:35:33 +0100

    linked

--- Java-Data-Structures/src/linkedlist/ListTest.java
+++ Java-Data-Structures/src/linkedlist/ListTest.java
@@ -97,11 +97,81 @@ public class ListTest {
     sll.clearList();
     out.println( "Convert to String: " + sll.toString() );
     
+    
     out.println("----------------------");
     out.println("DoubleLinkedList TEST!");
     out.println("----------------------");
 		
     DoubleLinkedList<Integer> dll = new DoubleLinkedList<>(); 
+    DoubleListNode<Integer> q;
+    out.println( "Empty: " + dll.isEmpty() );
+    // Insert at begin
+    for (int i = 0; i < 10; i++) {
+      dll.insertAtBegin(i);
+    }
+    out.println( "Empty: " + dll.isEmpty() );
+    // Print the list
+    for (q=dll.getHead(); q != null; q=q.getNext()) {
+      out.println( q.getData() + ", Next: " + q.getNext() );
+    }
+    // Remove first
+    for (int i = 0; i < 10; i++) {
+      dll.removeFirst();
+    }
+    
+    // -------------------------------------------------------------
+    // Insert at the end
+    for (int i = 10; i < 20; i++) {
+      dll.insertAtEnd(i);
+    }
+    out.println( "Empty: " + dll.isEmpty() );
+    // Print the list
+    for (q=dll.getHead(); q != null; q=q.getNext()) {
+      out.println( q.getData() + ", Next: " + q.getNext() );
+    }
+    // Remove the last
+    for (int i = 0; i < 10; i++) {
+      dll.removeLast();
+    }
+    out.println( "Empty: " + dll.isEmpty() );
+    
+    // -------------------------------------------------------------
+    out.println( "peekAt 0: " + dll.peekAt(0) );
+    for (int i = 0; i < 10; i++) {
+      dll.insertAtBegin(i);
+    }
+    out.println( "peekAt 0: " + dll.peekAt(0) );
+    dll.insert(100, 5);
+    dll.insert(100, 0);
+    dll.insert(100, 55);
+    out.println( "peekAt 0: " + dll.peekAt(0) );
+    
+    // Print the list
+    for (q=dll.getHead(); q != null; q=q.getNext()) {
+      out.println( q.getData() + ", Next: " + q.getNext() );
+    }
+    
+    // -------------------------------------------------------------
+    // Test getPosition(T data)
+    out.println( "9 From: " + dll.getPosition(9) + ", 4 From: " +  dll.getPosition(4) + ", 19 From: " + dll.getPosition(19) );
+    out.println( "101 From: " + dll.getPosition(101) );
+    
+     // Test removeFirst() and removeLast()
+    out.println( "Removed first: " + dll.removeFirst() );
+    out.println( "Removed last: " + dll.removeLast() );
+    
+    // Test getLength()
+    out.println( "The length of the list: " + dll.getLength() );
+    
+    // Test toString()
+    out.println( "Convert to String: " + dll.toString() );
+    
+    // Test peekAt(int position)
+    out.println( "@0: " + dll.peekAt(0) + ", @10: " + dll.peekAt(10) + ", @20: " + dll.peekAt(20) );
+    out.println( "@50: " + dll.peekAt(50) );
+    
+    dll.clearList();
+    out.println( "Convert to String: " + dll.toString() );
     
     // TODO
 		
