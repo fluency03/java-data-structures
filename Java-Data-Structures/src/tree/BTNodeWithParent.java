1dc0b4030ff5e00bf5a5e59396891103870015cd
cliu <fluency.03@gmail.com>
2016-01-10 15:50:31 +0100

    BTNodeWithParent.java

---
+++ Java-Data-Structures/src/tree/BTNodeWithParent.java
@@ -0,0 +1,27 @@
+/**
+ * Class: BinaryTreeNode with Parent
+ * Author: Chang LIU
+ */
+
+package tree;
+
+import static java.lang.System.out;
+
+public class BTNodeWithParent<T> {
+
+  private BinaryTreeNode<T> parent = null;
+  private BinaryTreeNode<T> left = null;
+  private BinaryTreeNode<T> right = null;
+  private T data = null;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
