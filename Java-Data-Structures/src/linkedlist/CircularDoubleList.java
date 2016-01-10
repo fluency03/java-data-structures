acb7703b749fa0d343376420c0ae8aa857bd9e6f
cliu <fluency.03@gmail.com>
2016-01-10 13:24:15 +0100

    linked

--- Java-Data-Structures/src/linkedlist/CircularDoubleList.java
+++ Java-Data-Structures/src/linkedlist/CircularDoubleList.java
@@ -94,9 +94,178 @@ public class CircularDoubleList<T> {
    * Insert node with several methods
    * --------------------------------
    */
+  // Insert a node at the front of the list
+  public void insertAtBegin(T data) {
+    DoubleListNode<T> newNode = new DoubleListNode<>(data);
+    
+    if (length == 0) {
+      newNode.setNext(newNode);
+      newNode.setPrev(newNode);
+      head = newNode;
+    } else {
+      newNode.setNext(head);
+      newNode.setPrev(head.getPrev());
+      head.getPrev().setNext(newNode);
+      head.setPrev(newNode);
+      head = newNode;
+    } 
+    length ++;
+    
+  }
+
+  // Insert a node at the end of the list
+  public void insertAtEnd(T data) {
+    DoubleListNode<T> newNode = new DoubleListNode<>(data);
+    
+    if (length == 0) {
+      newNode.setNext(newNode);
+      newNode.setPrev(newNode);
+      head = newNode;
+    } else {
+      newNode.setNext(head);
+      newNode.setPrev(head.getPrev());
+      head.getPrev().setNext(newNode);
+      head.setPrev(newNode);
+    }
+    length ++;
+    
+  }
+
+  // Insert a node at certain position 
+  public boolean insert(T data, int position) {
+    // Check the position
+    if (position < 0 || position >= length) {
+      out.println("The position " + position +" is out of range! " + "The length is " + this.length + "!");
+      return false;
+    }
+    
+    DoubleListNode<T> temp = head;
+    DoubleListNode<T> newNode = new DoubleListNode<>(data);
+    if (length == 0) {
+      newNode.setNext(newNode);
+      newNode.setPrev(newNode);
+      head = newNode;
+    } else if (position == 0) {
+      newNode.setNext(head);
+      newNode.setPrev(head.getPrev());
+      head.getPrev().setNext(newNode);
+      head.setPrev(newNode);
+      head = newNode;
+    } else {
+      
+      
+      // TODO
+      
+      for (int i=1; i<position; i++){
+        temp = temp.getNext();
+      }
+      newNode.setNext(temp.getNext());
+      newNode.setPrev(temp);
+      temp.getNext().setPrev(newNode);
+      temp.setNext(newNode);
+    }
+    
+    length ++;
+    return true;
+  }
   
+  /**
+   * --------------------------------
+   * Remove node with several methods
+   * --------------------------------
+   */
+  // Remove a node from the beginning of the list
+  public T removeFirst() {
+    if (length == 0) {
+      out.println("Nothing to be removed!");
+      return null;
+    } else if (length == 1) {
+      DoubleListNode<T> temp = head;
+      head = null;
+      tail = null;
+      length --;
+      return temp.getData();
+    }
+    
+    DoubleListNode<T> temp = head;
+    head = head.getNext();
+    head.setPrev(null);
+    length --;
+    return temp.getData();
+  }
   
+  // Remove a node from the end of the list
+  public T removeLast() {
+    if (length == 0) {
+      out.println("Nothing to be removed!");
+      return null;
+    } else if (length == 1) {
+      DoubleListNode<T> temp = head;
+      head = null;
+      tail = null;
+      length --;
+      return temp.getData();
+    }
+    
+    DoubleListNode<T> temp = tail;
+    tail = tail.getPrev();
+    tail.setNext(null);
+    length --;     
+    return temp.getData();
+  }
   
+  // Remove a node from certain position
+  public T removeFrom(int position) {
+    if (position < 0 || position >= length) {
+      out.println("The position " + position +" is out of range! " + "The length is " + this.length + "!");
+      return null;
+    }
+    
+    DoubleListNode<T> temp = head;
+//    DoubleListNode<T> removedNode = temp.getNext();
+    if (length == 0) {
+      out.println("Nothing to be removed!");
+      return null;
+    } else if (position == 0) {
+      head = head.getNext();
+      head.setPrev(null);
+      length --;
+      return temp.getData();
+    } else {
+      DoubleListNode<T> removedNode = temp.getNext();
+      for (int i=1; i<position; i++){
+        temp = temp.getNext();
+        removedNode = temp.getNext();
+      }
+      if (removedNode.getNext() == null) {
+        temp.setNext(null);
+        tail = temp;
+        length --;
+        return removedNode.getData(); 
+      }
+      temp.setNext(removedNode.getNext());
+      removedNode.getNext().setPrev(temp);
+      length --;
+      return removedNode.getData();
+    }  
+  }
+  
+  public String toString() {
+    String str = "";
+    if (length == 0) {
+      out.println("The list is empty!");
+      return str;
+    }
+    
+    DoubleListNode<T> temp = head;
+    DoubleListNode<T> p;
+    str = str + temp.getData();
+    while ((p = temp.getNext()) != null) {
+      str = str + "," + p.getData();
+      temp = temp.getNext();
+    }
+    return str;
+  }
   
   
   
