3b0ba6890a8f60a2774be25d2d3e73e680149f4e
Chang Liu <fluency.03@gmail.com>
2016-01-13 19:31:45 +0100

    src/tree/TreeTest.java

---
+++ Java-Data-Structures/src/tree/TreeTest.java
@@ -0,0 +1,17 @@
+/**
+ * Class: TreeTest
+ * Author: Chang LIU
+ */
+
+package tree;
+
+import static java.lang.System.out;
+
+public class TreeTest {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
