9857f4673005297795c0337deb29fa248a65e27b
Chang Liu <fluency.03@gmail.com>
2016-01-13 19:30:53 +0100

    src/queue/QueueTest.java

---
+++ Java-Data-Structures/src/queue/QueueTest.java
@@ -0,0 +1,17 @@
+/**
+ * Class: QueueTest
+ * Author: Chang LIU
+ */
+
+package queue;
+
+import static java.lang.System.out;
+
+public class QueueTest {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
