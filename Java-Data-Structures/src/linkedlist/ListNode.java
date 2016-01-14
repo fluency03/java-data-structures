91f875a66ba35ec76ca412140b96e005b43f6146
Chang Liu <fluency.03@gmail.com>
2016-01-14 16:42:56 +0100

    ListNode.java

--- Java-Data-Structures/src/linkedlist/ListNode.java
+++ Java-Data-Structures/src/linkedlist/ListNode.java
@@ -10,41 +10,48 @@ public class ListNode<T> {
   private ListNode<T> next = null;
   private T data = null;
 
-  // Create an empty node
   public ListNode() { }
 
-  // Create an node with data specified
   public ListNode(T data) {
     this.data = data;
   }
   
-  // Create an node with data specified
   public ListNode(T data, ListNode<T> next) {
     this.data = data;
     this.next = next;
   }
 
-  // Obtain the next node
+  /*
+   *  Obtain the next node
+   */
   public ListNode<T> getNext() {
     return next;
   }
 
-  // Set the next node
+  /*
+   *  Set the next node
+   */
   public void setNext(ListNode<T> next) {
     this.next = next;
   }
 
-  // Obtain the data of current node
+  /*
+   *  Obtain the data of current node
+   */
   public T getData() {
     return data;
   }
 
-  // Set data of current node
+  /*
+   *  Set data of current node
+   */
   public void setData(T data) {
     this.data = data;
   }
 
-  // Convert the node data into String
+  /*
+   *  Convert the node data into String
+   */
   public String toString() {
     String str = "";
     return str + data;
