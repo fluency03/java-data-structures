8200582dd50c9cd8a813ab5a7163f299f566d9d3
cliu <fluency.03@gmail.com>
2016-01-14 01:17:15 +0100

    queue/ArrayQueue.java

--- Java-Data-Structures/src/queue/ArrayQueue.java
+++ Java-Data-Structures/src/queue/ArrayQueue.java
@@ -8,9 +8,15 @@ package queue;
 //import static java.lang.System.out;
 
 public class ArrayQueue<T> {
-
+	
+	private T[] array;
+	private int head = 0;
+	private int tail = 0;
+	private boolean full = false;
   
-  
+  public ArrayQueue(int size) {
+  	array = (T[])new Object[size];
+	}
   
   
   
