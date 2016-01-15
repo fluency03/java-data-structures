1f97c8d702087d645f80c9135d80b194e8b0c433
Chang Liu <fluency.03@gmail.com>
2016-01-15 12:10:56 +0100

    ArrayQueue

--- Java-Data-Structures/src/queue/ArrayQueue.java
+++ Java-Data-Structures/src/queue/ArrayQueue.java
@@ -5,6 +5,10 @@
 
 package queue;
 
+import static java.lang.System.out;
+
+import linkedlist.ListNode;
+
 //import static java.lang.System.out;
 
 public class ArrayQueue<T> {
@@ -20,11 +24,97 @@ public class ArrayQueue<T> {
 	}
   
   /*
+   * Enqueue a data into the tail
+   */
+  public void enqueue(T data) {
+    if (!full) {
+      array[tail = ((++tail) % array.length) ] = data;
+    }
+    if (head == tail) {
+      full = true;
+    }
+  }
+  
+  /*
+   * Dequeue a data from the head
+   */
+  public T dequeue() {
+    if ((head == tail) && !full) {
+      return null;
+    } else if (full == true) {
+      full = false;
+    }
+    T removed = array[head];
+    array[head] = null;
+    head = (++head) % array.length;
+    return (removed);
+  }
+  
+  /*
+   * Return the head data
+   */
+  public T getHead() {
+    return array[head];
+  }
+                
+  /*
+   * Return the tail data
+   */
+  public T getTail() {
+    return array[tail];
+  }
+  
+  /*
    *  Check Empty
    */
   public boolean isEmpty() {
     return ((head == tail) && !full);
   }
+  
+  /*
+   * Return the length of the queue
+   */
+  public int getLength() {
+    if (full) {
+      return array.length;
+    } else if (head == tail) {
+      return 0;
+    } else if (tail >= head) {
+      return (tail - head);
+    } else {
+      return (array.length - head + tail);
+    }
+  }
+  
+  /* 
+   * Convert the queue into a String
+   */
+  public String toString() {
+    String str = "";
+
+    // TODO
+    
+    return str;
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
   
   
