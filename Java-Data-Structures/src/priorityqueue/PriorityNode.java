b47fe2e3848d75f57ecf2b565df6887c336b06e9
Chang Liu <fluency.03@gmail.com>
2016-01-15 19:49:22 +0100

    src/priorityqueue/PriorityNode.java

--- Java-Data-Structures/src/priorityqueue/PriorityNode.java
+++ Java-Data-Structures/src/priorityqueue/PriorityNode.java
@@ -5,17 +5,40 @@
 
 package priorityqueue;
 
-public class PriorityNode<T> {
+import linkedlist.ListNode;
 
+public class PriorityNode<T> extends ListNode<T> {
+
+  private PriorityNode<T> next = null;
+  private T data = null;
+  private int priority = 0;
+  
+  public PriorityNode() { }
+
+  public PriorityNode(T data) {
+    this.data = data;
+  }
+  
+  public PriorityNode(T data, PriorityNode<T> next) {
+    this.data = data;
+    this.next = next;
+  }
 	
+  public PriorityNode(T data, int priority) {
+    this.data = data;
+    this.priority = priority;
+  }
 	
+  public PriorityNode(PriorityNode<T> next, int priority) {
+    this.next = next;
+    this.priority = priority;
+  }
 	
-	
-	
-	
-	
-	
-	
+  public PriorityNode(T data, PriorityNode<T> next, int priority) {
+    this.data = data;
+    this.next = next;
+    this.priority = priority;
+  }
 	
 	
 	
