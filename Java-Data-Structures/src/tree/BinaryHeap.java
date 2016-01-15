33407c2c466940f139496ca3e5fc9cac3905b032
Chang Liu <fluency.03@gmail.com>
2016-01-15 21:11:00 +0100

    Binary Tree

---
+++ Java-Data-Structures/src/tree/BinaryHeap.java
@@ -0,0 +1,10 @@
+/**
+ * Class: BinaryHeap
+ * Author: Chang LIU
+ */
+
+package tree;
+
+public class BinaryHeap<T> {
+
+}
---
+++ Java-Data-Structures/src/tree/BinarySearchTree.java
@@ -0,0 +1,12 @@
+/**
+ * Class: BinarySearchTree
+ * Author: Chang LIU
+ */
+
+package tree;
+
+public class BinarySearchTree<T> {
+
+  
+  
+}
