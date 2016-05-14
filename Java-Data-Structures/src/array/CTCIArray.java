ddbfbf1f30afcfeb8aeec393f3435c80cd3df5fe
cliu <fluency.03@gmail.com>
2016-05-14 16:05:07 +0200

    array/

---
+++ Java-Data-Structures/src/array/CTCIArray.java
@@ -0,0 +1,32 @@
+/**
+ * Class: HashEntry
+ * Author: Chang LIU
+ */
+
+package array;
+
+public class CTCIArray {
+
+  /*
+    join Strings
+   */
+  public String joinWords(String[] words){
+    StringBuffer sentence = new StringBuffer();
+    for (String w: words) {
+      sentence.append(w);
+    }
+    return sentence.toString();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+}
