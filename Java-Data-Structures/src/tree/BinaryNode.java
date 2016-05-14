f8482a9cd84a10da1478a6b1e2aa5b902775bb77
cliu <fluency.03@gmail.com>
2016-05-14 18:50:33 +0200

    dfs bfs

--- Java-Data-Structures/src/tree/BinaryNode.java
+++ Java-Data-Structures/src/tree/BinaryNode.java
@@ -5,6 +5,8 @@
 
 package tree;
 
+import java.util.*;
+
 public class BinaryNode<T> {
 
   private T data;
@@ -38,12 +40,43 @@ public class BinaryNode<T> {
     this.visited = visited;
   }
 
-  
+  public void bfs() {
+    Queue<BinaryNode<T>> queue = new LinkedList<>();
+    queue.add(this);
+    this.setVisited(true);
+    while(!queue.isEmpty()) {
+      BinaryNode<T> node = queue.remove();
+      BinaryNode<T> left_node = node.getLeft();
+      BinaryNode<T> right_node = node.getLeft();
+      if (left_node != null && left_node.isVisited() == false) {
+        left_node.setVisited(true);
+        queue.add(left_node);
+      }
+      if (right_node != null && right_node.isVisited() == false) {
+        right_node.setVisited(true);
+        queue.add(right_node);
+      }
+    }
+  }
 
 
-
-
-
-
+  public void dfs() {
+    Stack<BinaryNode<T>> stack = new Stack<>();
+    stack.push(this);
+    this.setVisited(true);
+    while(!stack.isEmpty()) {
+      BinaryNode<T> node = stack.pop();
+      BinaryNode<T> left_node = node.getLeft();
+      BinaryNode<T> right_node = node.getLeft();
+      if (left_node != null && left_node.isVisited() == false) {
+        left_node.setVisited(true);
+        stack.push(left_node);
+      }
+      if (right_node != null && right_node.isVisited() == false) {
+        right_node.setVisited(true);
+        stack.push(right_node);
+      }
+    }
+  }
 
 }
