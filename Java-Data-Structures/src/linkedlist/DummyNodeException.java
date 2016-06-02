cb683267e45fd47b174c0bf69c93d52de798d297
cliu <fluency.03@gmail.com>
2016-06-02 19:07:55 +0200

    DummyNodeException

---
+++ Java-Data-Structures/src/linkedlist/DummyNodeException.java
@@ -0,0 +1,12 @@
+/**
+ * Class: DummyNodeException
+ * Author: Chang LIU
+ */
+
+package linkedlist;
+
+public class DummyNodeException extends LinkedListException {
+   public DummyNodeException(String msg){
+      super(msg);
+   }
+}
