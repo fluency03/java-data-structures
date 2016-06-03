55488f574857f903300d1b4e528d9daddd6613c2
cliu <fluency.03@gmail.com>
2016-06-03 14:50:42 +0200

    EmptyListException.java

---
+++ Java-Data-Structures/src/linkedlist/EmptyListException.java
@@ -0,0 +1,13 @@
+/**
+ * Class: DummyNodeException
+ * Author: Chang LIU
+ */
+
+package linkedlist;
+
+@SuppressWarnings("serial")
+public class EmptyListException extends Exception {
+   public EmptyListException(String msg){
+      super(msg);
+   }
+}
